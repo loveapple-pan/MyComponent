4cf563ad2ea0c69218a89980193a9485525fccb6
loveapple-pan <1104483764.com>
2018-08-15 16:57:19 +0800

    使用template模板&&递归组件&&依赖注入

---
+++ tree/tree1.vue
@@ -0,0 +1,104 @@
+<template>
+    <ul>
+        <li v-for="(item,index) in nodes" :key="index">
+            <span 
+            @click="handleExpand(item)"
+            class="icon"
+            :class="[item.children&&(item.expand?'triangle-bottom':'triangle-right')]"
+            ></span>
+            <span @click="selected(item)" class="title" :class="{'tree-active':item == currentActive.active}">{{item.name}}</span>
+            <tree1 v-if="item.children" :nodes="item.children" v-show="item.expand"></tree1>
+        </li>
+    </ul>
+</template>
+
+
+<script>
+export default {
+  props: {
+    nodes: Array,
+    deep: {
+      type: Number,
+      default: 0
+    }
+  },
+  inject: ["currentActive", "active"],
+  data() {
+    return {};
+  },
+  name: "Tree1",
+  methods: {
+    handleExpand(currentNode) {
+      //节点展开状态
+      if (currentNode.hasOwnProperty("expand")) {
+        //如果是可展开的 则执行展开与否操作
+        currentNode.expand = !currentNode.expand;
+      }
+    },
+    selected(currentNode) {
+      //节点选中状态
+      if (!currentNode.selected) {
+        this.active(currentNode);
+        currentNode.selected = true;
+      } else {
+        this.active(false);
+        currentNode.selected = false;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  padding: 5px 30px;
+  margin: 0;
+  list-style: none;
+}
+ul > li {
+  position: relative;
+}
+span.title {
+  user-select: none;
+  color: #515a6e;
+  padding: 1px 15px;
+  cursor: pointer;
+  font-family: 微软雅黑;
+  font-size: 16px;
+  border-radius: 3px;
+  display: inline-block;
+  transition: background-color 0.2s ease-in-out;
+  width: 280px;
+  height: 40px;
+  line-height: 40px;
+  background: #cccccc;
+  margin-bottom: 10px;
+}
+.triangle-bottom::before,
+.triangle-right::before {
+  font-weight: 400;
+  position: absolute;
+  content: "";
+  height: 0;
+  width: 0;
+  border: 7px solid transparent;
+  top: 13px;
+  bottom: 0;
+  left: -20px;
+  right: 0;
+  /* margin: auto 0; */
+}
+
+.triangle-right::before {
+  border-left-color: rgba(0, 0, 0, 0.75);
+}
+.triangle-bottom::before {
+  border-top-color: rgba(0, 0, 0, 0.75);
+}
+span:hover {
+  background: #eaf4fe;
+}
+span.tree-active {
+  background: #d5e8fc;
+}
+</style>
